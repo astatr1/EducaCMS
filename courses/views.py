107359cb02e1f5333016447d8bd425d3bc8f860b
astatr1 <astatr1@yandex.ru>
2024-02-02 20:57:38 +0300

    Добавлена общедоступная область отображения курсов: все курсы и детальная информация о каждом.

--- courses/views.py
+++ courses/views.py
@@ -1,4 +1,5 @@
 from braces.views import CsrfExemptMixin, JsonRequestResponseMixin
+from django.views.generic import DetailView
 from django.views.generic.list import ListView
 from django.views.generic.edit import CreateView, UpdateView, DeleteView
 from django.views.generic.base import TemplateResponseMixin, View
@@ -7,7 +8,8 @@ from django.shortcuts import redirect, get_object_or_404
 from django.urls import reverse_lazy
 from django.forms.models import modelform_factory
 from django.apps import apps
-from .models import Course, Module, Content
+from django.db.models import Count
+from .models import Course, Module, Content, Subject
 from .forms import ModuleFormSet
 
 
@@ -157,3 +159,23 @@ class ContentOrderView(CsrfExemptMixin, JsonRequestResponseMixin, View):
                                    module__course__owner=request.user
                                    ).update(order=order)
             return self.render_json_response({'saved': 'OK'})
+
+
+class CourseListView(TemplateResponseMixin, View):
+    model = Course
+    template_name = 'courses/course/list.html'
+
+    def get(self, request, subject=None):
+        subjects = Subject.objects.annotate(total_coureses=Count('courses'))
+        courses = Course.objects.annotate(total_modules=Count('modules'))
+        if subject:
+            subject = get_object_or_404(Subject, slug=subject)
+            courses = courses.filter(subject=subject)
+        return self.render_to_response({'subjects': subjects,
+                                      'subject': subject,
+                                      'courses': courses})
+
+
+class CourseDetailView(DetailView):
+    model = Course
+    template_name = 'courses/course/detail.html'
