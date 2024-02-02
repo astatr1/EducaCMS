ced62f3784dd9b61cbd787c23deae4e22fe294b0
astatr1 <astatr1@yandex.ru>
2024-02-02 20:20:49 +0300

    Добавлена функциональность перетаскивания модулей и контента в курсе с помощью JavaScript для переупорядочивания.

--- courses/views.py
+++ courses/views.py
@@ -1,3 +1,4 @@
+from braces.views import CsrfExemptMixin, JsonRequestResponseMixin
 from django.views.generic.list import ListView
 from django.views.generic.edit import CreateView, UpdateView, DeleteView
 from django.views.generic.base import TemplateResponseMixin, View
@@ -138,3 +139,21 @@ class ModuleContentListView(TemplateResponseMixin, View):
         module = get_object_or_404(Module, id=module_id,
                                    course__owner=request.user)
         return self.render_to_response({'module': module})
+
+
+class ModuleOrderView(CsrfExemptMixin, JsonRequestResponseMixin, View):
+    def post(self, request):
+        for id, order in self.request_json.items():
+            Module.objects.filter(id=id,
+                                  course__owner=request.user
+                                  ).update(order=order)
+            return self.render_json_response({'saves': 'OK'})
+
+
+class ContentOrderView(CsrfExemptMixin, JsonRequestResponseMixin, View):
+    def post(self, request):
+        for id, order in self.request_json.items():
+            Content.objects.filter(id=id,
+                                   module__course__owner=request.user
+                                   ).update(order=order)
+            return self.render_json_response({'saved': 'OK'})
