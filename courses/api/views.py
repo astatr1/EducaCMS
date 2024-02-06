4616bb9a5589d32878d86333b3cc8919b3c049af
astatr1 <astatr1@yandex.ru>
2024-02-06 21:28:35 +0300

    Добавлена обработка аутентификации на основе разрешений к представлению записи на курс по API.

--- courses/api/views.py
+++ courses/api/views.py
@@ -1,6 +1,8 @@
 from rest_framework import generics
 from rest_framework.views import APIView
 from rest_framework.response import Response
+from rest_framework.authentication import BasicAuthentication
+from rest_framework.permissions import IsAuthenticated
 from courses.models import Subject, Course
 from courses.api.serializers import SubjectSerializer
 from django.shortcuts import get_object_or_404
@@ -20,6 +22,10 @@ class SubjectDetailView(generics.RetrieveAPIView):
 
 class CourseEnrollView(APIView):
     """Обрабатывает зачисление пользователей на курс"""
+    authentication_classes = [BasicAuthentication]
+    # доступно только для авторизованных пользователей
+    permission_classes = [IsAuthenticated]
+
     def post(self, request, pk, format=None):
         course = get_object_or_404(Course, pk=pk)
         course.students.add(request.user)
