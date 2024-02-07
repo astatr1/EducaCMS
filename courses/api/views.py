754ebca0b157ba5955c626e4d0d758cb43b00af6
astatr1 <astatr1@yandex.ru>
2024-02-07 20:42:18 +0300

    Добавлена сериализация содержимого курса.

--- courses/api/views.py
+++ courses/api/views.py
@@ -1,13 +1,13 @@
 from rest_framework import generics
-from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework.authentication import BasicAuthentication
 from rest_framework.permissions import IsAuthenticated
 from rest_framework import viewsets
 from rest_framework.decorators import action
 from courses.models import Subject, Course
-from courses.api.serializers import SubjectSerializer, CourseSerializer
-from django.shortcuts import get_object_or_404
+from courses.api.serializers import (SubjectSerializer, CourseSerializer,
+                                     CourseWithContentSerializer)
+from courses.api.permissions import IsEnrolled
 
 
 class SubjectListView(generics.ListAPIView):
@@ -34,3 +34,12 @@ class CourseViewSet(viewsets.ReadOnlyModelViewSet):
         course = self.get_object()
         course.students.add(request.user)
         return Response({'enrolled': True})
+
+    @action(detail=True,
+            methods=['get'],
+            serializer_class=CourseWithContentSerializer,
+            authentication_classes=[BasicAuthentication],
+            permission_classes=[IsAuthenticated, IsEnrolled])
+    def contents(self, request, *args, **kwargs):
+        return self.retrieve(request, *args, **kwargs)
+
