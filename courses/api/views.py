ea9eb0270850ab2a372178e84032c6edd409b175
astatr1 <astatr1@yandex.ru>
2024-02-07 20:17:50 +0300

    Создан набор представлений и маршрутизаторов. Добавлена проверка разрешений на уровне экземпляров к доступу сведений по API.

--- courses/api/views.py
+++ courses/api/views.py
@@ -3,8 +3,10 @@ from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework.authentication import BasicAuthentication
 from rest_framework.permissions import IsAuthenticated
+from rest_framework import viewsets
+from rest_framework.decorators import action
 from courses.models import Subject, Course
-from courses.api.serializers import SubjectSerializer
+from courses.api.serializers import SubjectSerializer, CourseSerializer
 from django.shortcuts import get_object_or_404
 
 
@@ -20,13 +22,15 @@ class SubjectDetailView(generics.RetrieveAPIView):
     serializer_class = SubjectSerializer
 
 
-class CourseEnrollView(APIView):
-    """Обрабатывает зачисление пользователей на курс"""
-    authentication_classes = [BasicAuthentication]
-    # доступно только для авторизованных пользователей
-    permission_classes = [IsAuthenticated]
+class CourseViewSet(viewsets.ReadOnlyModelViewSet):
+    queryset = Course.objects.all()
+    serializer_class = CourseSerializer
 
-    def post(self, request, pk, format=None):
-        course = get_object_or_404(Course, pk=pk)
+    @action(detail=True,
+            methods=['post'],
+            authentication_classes=[BasicAuthentication],
+            permission_classes=[IsAuthenticated])
+    def enroll(self, request, *args, **kwargs):
+        course = self.get_object()
         course.students.add(request.user)
         return Response({'enrolled': True})
